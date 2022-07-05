59ea41ece8e8aceeda5775cf0ee7ca5dfc8a1708
rolysr <rolsanchez@yandex.com>
2022-07-05 17:26:32 -0400

    Added Hidato and Pos record types

--- src/Utils.hs
+++ src/Utils.hs
@@ -3,3 +3,9 @@ apply a x1 y1 v = [[ if i==x1 && j==y1 then v else a !! i !! j | j<-[0..(length 
 
 dirs :: [[Int]]
 dirs=[[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+
+#Position record type
+data Pos = nilPos | Pos {row :: Int, column :: Int}
+
+#Hidato record type
+data Hidato = nilHidato | Hidato {board :: [[Int]], posMin :: Pos, posMax :: Pos, minValue :: Int, maxValue :: Int, width :: Int, height :: Int}

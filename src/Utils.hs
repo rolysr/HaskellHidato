add32ceb49a005ed5b12a5656adfc2f6d6d7b1a7
dmga44 <davidmga44@gmail.com>
2022-07-05 18:42:13 +0200

    commit fix/erasing-double-declarations

--- src/Utils.hs
+++ src/Utils.hs
@@ -23,7 +23,6 @@ updateList a x v =
 dirs :: [[Int]]
 dirs=[[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
 
-
 --Position record type
 data Pos = Pos {row :: Int, column :: Int} | NilPos deriving (Show)
 
@@ -82,9 +81,3 @@ bfs mk a = bfs_part2 mk (markPositions mk a)
 ---Function to check if the graph is connected or not
 isConnected :: [[Int]] -> Bool
 isConnected a = full_minus_1_matrix (length a) (length (a!!0)) == bfs (position_distinct_from_minus_1 a 0) a
-
--- #Position record type
-data Pos = nilPos | Pos {row :: Int, column :: Int}
-
--- #Hidato record type
-data Hidato = nilHidato | Hidato {board :: [[Int]], posMin :: Pos, posMax :: Pos, minValue :: Int, maxValue :: Int, width :: Int, height :: Int}
